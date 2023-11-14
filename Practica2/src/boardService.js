8966fa039fa44f662f71a30012bc4b388f651d30
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2023-11-14 18:13:28 +0100

    nombres variables

--- Practica2/src/boardService.js
+++ Practica2/src/boardService.js
@@ -1,19 +1,7 @@
 const productos = new Map();
 let nextId = 0;
 addProduct({
-    prodNombre: "AiPhone 15 Pro", precio: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
-});
-addProduct({
-    prodNombre: "AiPhone 15 Pro", precio: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
-});
-addProduct({
-    prodNombre: "AiPhone 15 Pro", precio: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
-});
-addProduct({
-    prodNombre: "AiPhone 15 Pro", precio: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
-});
-addProduct({
-    prodNombre: "AiPhone 15 Pro", precio: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
+    nombreProd: "AiPhone 15 Pro", precioProd: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
 });
 
 export function addProduct(product) {

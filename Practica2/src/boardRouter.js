c4540329da3ce3f4305667c60fc4ef5f41cef901
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2023-11-14 16:33:44 +0100

    routers

---
+++ Practica2/src/boardRouter.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import * as boardService from './boardService.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        posts: boardService.getProducts()
+    });
+});
+
+
+router.post('/post/new', (req, res) => {
+    let { user, title, text } = req.body;
+    boardService.addProduct({ user, title, text });
+    res.render('saved_post');
+});
+
+router.get('/post/:id', (req, res) => {
+    let post = boardService.getProducts(req.params.id);
+    res.render('show_post', { post });
+});
+
+router.get('/post/:id/delete', (req, res) => {
+    boardService.deleteProduct(req.params.id);
+    res.render('deleted_post');
+});
+
+export default router;
---
+++ Practica2/src/boardService.js
@@ -0,0 +1,22 @@
+const posts = new Map();
+let nextId = 0;
+addProducts({
+    prodNombre: "AiPhone 15 Pro", precio: "1219", imgURL: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg"
+});
+
+export function addProduct(post) {
+    let id = nextId++;
+    post.id = id.toString();
+    posts.set(post.id, post);
+}
+export function deleteProduct(id) {
+    posts.delete(id);
+}
+export function getProducts() {
+    return [...posts.values()];
+}
+export function getProducts(id) {
+    return posts.get(id);
+}
+
+

8fc0e41f20808be00e32c24c9108c7c66d3cad60
luis <luis@a.com>
2023-11-21 17:57:21 +0100

    desc añadida

--- Practica2/src/boardService.js
+++ Practica2/src/boardService.js
@@ -7,7 +7,7 @@ addProduct({
 
 addProduct({
     nombreProd: "AiPhone 15 Pro", precioProd: "1219,00", imgProd: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/1838-apple-iphone-15-pro-256gb-titanio-azul-libre.jpg", imgProd2: "https://thumb.pccomponentes.com/w-530-530/articles/1077/10777860/3456-apple-iphone-15-pro-256gb-titanio-azul-libre-mejor-precio.jpg",
-    descProd: "muy bonito", tipoProd: "Movil", cargaProd: "USB-C", capProd: "128 GB", colorProd: "#484E6E", nombreColor: "Titanio Azul"
+    tipoProd: "Movil", cargaProd: "USB-C", capProd: "128 GB", colorProd: "#484E6E", nombreColor: "Titanio Azul", descProd: "\n- Telefono móvil  \n- 6,7″ o 6,1″ Pantalla Super Retina XDR \n- Tecnología ProMotion\n- Pantalla siempre activa\n- Titanio con parte trasera de vidrio mate texturizado\n- Botón Acción\n- Dynamic Island\n- Chip A17 Pro con GPU de 6 núcleos\n- Sistema de cámaras Pro\n- Principal de 48 Mpx, Ultra gran angular, Teleobjetivo\n- Fotos de superalta resolución (24 y 48 Mpx)\n- Hasta 29 horas de reproducción de video\n- Compatibilidad con USB 3 para transferencias hasta x20 más rápidas\n- Face ID"
 });
 
 export function addProduct(product) {

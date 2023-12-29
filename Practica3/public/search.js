c4b4e9231e0825329f706b9f76b237ef675e3679
luis <luis@a.com>
2023-12-29 13:36:32 +0100

    avance search

--- Practica3/public/search.js
+++ Practica3/public/search.js
@@ -1,30 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const searchInput = document.getElementById('searchInput');
-    const listaResultados = document.getElementById('listaResultados');
-
-    // Lista de productos (simulación)
-    const productos = [Producto1, producto2, producto3];
-
-    // Función para mostrar los resultados filtrados
-    function mostrarResultados(filtro) {
-        const resultadosFiltrados = productos.filter(producto =>
-            producto.toLowerCase().includes(filtro.toLowerCase())
-        );
-
-        // Limpiar lista de resultados
-        listaResultados.innerHTML = '';
-
-        // Mostrar resultados filtrados en la lista
-        resultadosFiltrados.forEach(resultado => {
-            const li = document.createElement('li');
-            li.textContent = resultado;
-            listaResultados.appendChild(li);
-        });
-    }
-
-    // Evento al escribir en el campo de búsqueda
-    searchInput.addEventListener('input', function (event) {
-        const textoBusqueda = event.target.value;
-        mostrarResultados(textoBusqueda);
-    });
-});
+async function search(input){
+    const response = await fetch("/get-productos");
+    let productos = await response.json();
+    productos = productos.filter(producto => producto.nombreProd.toLowerCase().includes(input.value.toLowerCase()));
+    console.log(productos);
+}

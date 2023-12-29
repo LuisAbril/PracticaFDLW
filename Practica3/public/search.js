2a9303f0448e9266e2581cf4371459d7086c7579
luis <luis@a.com>
2023-12-29 13:52:07 +0100

    search funcional

--- Practica3/public/search.js
+++ Practica3/public/search.js
@@ -1,6 +1,20 @@
+
+function waitingSearch(input) {
+    var typingTimer;
+    var endTypingTimer = 800; //ms
+    input.addEventListener("input", function () {
+        clearTimeout(typingTimer);
+
+        typingTimer = setTimeout(function () {
+            search(input);
+        }, endTypingTimer);
+
+    })
+}
+
 async function search(input){
     const response = await fetch("/get-productos");
     let productos = await response.json();
     productos = productos.filter(producto => producto.nombreProd.toLowerCase().includes(input.value.toLowerCase()));
-    console.log(productos);
+    console.log(input.value)
 }
